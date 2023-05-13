11ce21a28bac0d2798520e8ea2832c2070cf27c9
Mauricio Cucco <santiago.cucco@externos-ar.cencosud.com>
2023-05-13 20:17:46 -0300

    Start project setup

---
+++ src/app/core/services/api/api.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ApiService {
+	constructor(private httpClient: HttpClient) {}
+
+	public get<T>(endpoint: string, params = {}): Observable<T> {
+		return this.httpClient.get<T>(environment.apiUrl + endpoint, {
+			params,
+		});
+	}
+
+	public post<T>(endpoint: string, data: unknown): Observable<T> {
+		return this.httpClient.post<T>(environment.apiUrl + endpoint, data);
+	}
+
+	public put<T>(endpoint: string, data: unknown): Observable<T> {
+		return this.httpClient.put<T>(environment.apiUrl + endpoint, data);
+	}
+
+	public patch<T>(endpoint: string, data: unknown): Observable<T> {
+		return this.httpClient.patch<T>(environment.apiUrl + endpoint, data);
+	}
+
+	public delete<T>(endpoint: string): Observable<T> {
+		return this.httpClient.delete<T>(environment.apiUrl + endpoint);
+	}
+}
