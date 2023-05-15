5b559fdd1738b5dafc60e9ee0e135bf675be7835
Mauricio Cucco <santiago.cucco@externos-ar.cencosud.com>
2023-05-15 15:52:58 -0300

    Add date picker

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -3,9 +3,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
 	declarations: [],
-	exports: [MatCardModule, MatInputModule, MatSelectModule, MatTableModule],
+	exports: [
+		MatCardModule,
+		MatInputModule,
+		MatSelectModule,
+		MatTableModule,
+		MatDatepickerModule,
+		MatNativeDateModule,
+	],
 })
 export class AngularMaterialModule {}
