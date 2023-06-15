8ef62aad8cb196c6e662609ba856630fdb2bf969
Mauricio Cucco <santiago.cucco@externos-ar.cencosud.com>
2023-06-15 10:52:42 -0300

    Add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { CoreModule } from './core/core.module';
 
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { UnauthorizedInterceptor } from './core/interceptors/unauthorized.interceptor';
 
 @NgModule({
 	declarations: [AppComponent],
@@ -17,7 +20,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 		BrowserAnimationsModule,
 		SharedModule,
 	],
-	providers: [],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: UnauthorizedInterceptor,
+			multi: true,
+		},
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
