75a2998269cab9719aa0c83bd46f95f9bb02fafa
Mauricio Cucco <santiago.cucco@externos-ar.cencosud.com>
2023-10-11 00:26:32 -0300

    Fix render bug

--- src/app/core/services/qualifications/qualifications.service.ts
+++ src/app/core/services/qualifications/qualifications.service.ts
@@ -303,8 +303,11 @@ export class QualificationsService {
 		if (!student) this.cleanAlphabet.next(true);
 
 		if (thereIsDataAvailable) {
-			this.selectedSubjectId.set(subject ?? 0);
+			subject !== this.selectedSubjectId()
+				? this.filterBySubject(subject)
+				: null;
 			!subject ? this.filterSignals(filtersChanges) : null; // Para q no se limpien los estudiantes al cambiar de materia en Mobile
+			this.selectedSubjectId.set(subject ?? 0);
 		}
 	}
 
@@ -316,6 +319,24 @@ export class QualificationsService {
 		this.filterValues(student);
 		this.filterValues(task, 'Tasks');
 		this.filterValues(exam, 'Exams');
+	}
+
+	private filterBySubject(subjectId: number) {
+		this.cleanShow(this.tasks);
+		this.cleanShow(this.exams);
+
+		this.tasks.mutate(tasks =>
+			tasks.forEach(task => {
+				if (subjectId && task.subject.id !== subjectId)
+					task.show = false;
+			})
+		);
+		this.exams.mutate(exams =>
+			exams.forEach(exam => {
+				if (subjectId && exam.subject.id !== subjectId)
+					exam.show = false;
+			})
+		);
 	}
 
 	public create(
