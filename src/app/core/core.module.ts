eb8503addacaba0952ee42aa11d95503abed261f
Mauricio Cucco <mscucco@hotmail.com>
2024-08-17 18:38:47 -0300

    Update angular

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-	declarations: [],
-	imports: [HttpClientModule],
-})
+@NgModule({ declarations: [], imports: [], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class CoreModule {
 	constructor(@Optional() @SkipSelf() core: CoreModule) {
 		if (core) {
